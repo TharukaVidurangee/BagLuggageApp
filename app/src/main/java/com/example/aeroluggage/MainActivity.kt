7ae9ff32338c8b3fed18fd82ac1638d1e614e5d3
Samarasinghe GCS <120781230+CSSamarasinghe@users.noreply.github.com>
2024-07-31 16:27:57 +0530

    Wednesday-Changes

--- app/src/main/java/com/example/aeroluggage/MainActivity.kt
+++ app/src/main/java/com/example/aeroluggage/MainActivity.kt
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.Toolbar
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
 
         // Delay of 3 seconds before switching to SecondActivity
         Handler(Looper.getMainLooper()).postDelayed({
