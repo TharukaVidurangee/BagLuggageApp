30bac38995a54295a62357e6eede61936f0a76f7
TharukaVidurangee <tharukaperera01@gmail.com>
2024-08-02 15:03:22 +0530

    setting up the SQLite DB

---
+++ app/src/main/java/com/example/aeroluggage/Tag.kt
@@ -0,0 +1,3 @@
+package com.example.aeroluggage
+
+data class Tag(val id:Int, val bagtag: String, val room: String)
--- app/src/main/java/com/example/aeroluggage/barcode_screen.kt
+++ app/src/main/java/com/example/aeroluggage/barcode_screen.kt
@@ -4,13 +4,16 @@ package com.example.aeroluggage
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.aeroluggage.databinding.ActivityBarcodeScreenBinding
 import com.example.aeroluggage.databinding.ActivityMainBinding
+import com.example.sqlitedbaeroluggage.TagAdapter
 
 
 class barcode_screen : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityBarcodeScreenBinding
 
     //initializing the database helper
     private lateinit var db:TagDatabaseHelper
@@ -20,7 +23,7 @@ class barcode_screen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityBarcodeScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         //setting up the db
@@ -34,17 +37,31 @@ class barcode_screen : AppCompatActivity() {
         binding.tagRecyclerView.adapter = tagAdapter
 
 
-
         binding.addButton.setOnClickListener {
-            val intent = Intent(this, AddTag::class.java)
+            val intent = Intent(this, barcode_screen::class.java)
             startActivity(intent)
         }
+
+
+
+        binding.saveButton.setOnClickListener{
+            val room = binding.roomEditText.text.toString()
+            val bagtag = binding.tagEditText.text.toString()
+            val tag = Tag(0, room, bagtag)
+            db.insertTag(tag)
+            finish()
+            Toast.makeText(this, "Bag Tag saved", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
     //refresh data method, onResume function refresh the data automatically
     override fun onResume() {
         super.onResume()
         tagAdapter.refreshData(db.getAllTags())
     }
+
+
 }
 
 
