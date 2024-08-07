4cc83f7b84b407283f242694d4cebbcb6cd18ccb
TharukaVidurangee <tharukaperera01@gmail.com>
2024-08-07 12:05:00 +0530

    a few initial commits

--- app/src/main/java/com/example/aeroluggage/ApiService.kt
+++ app/src/main/java/com/example/aeroluggage/ApiService.kt
@@ -2,6 +2,7 @@ package com.example.aeroluggage // Change to your actual package name
 
 import RoomDataItem
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
@@ -9,7 +10,7 @@ import retrofit2.http.POST
 
 interface ApiService {
     @FormUrlEncoded
-    @POST("Easypass_revamp/Login/GetLoginValidationForStoragRoom")
+    @POST("Easypass_revamp/Login/GetLoginValidationForStoragRoom")  //for the login authentication
     fun login(
         @Field("StaffNo") staffNo: String,
         @Field("StaffPassword") staffPassword: String
@@ -17,7 +18,17 @@ interface ApiService {
 
     @GET("Easypass_revamp/Home/GetStorageRoomList") // Ensure this is the correct endpoint
     fun getStorageRoomList(): Call<List<RoomDataItem>>
+
+    @POST("")     //To sync data from the local SQLite DB to the central DB
+    fun syncData(@Body syncData: SyncData): Call<SyncData>
 }
+
+
+
+
+
+
+
 
 
 
---
+++ app/src/main/java/com/example/aeroluggage/SyncData.kt
@@ -0,0 +1,21 @@
+package com.example.aeroluggage
+
+data class SyncData(
+    val AddedDate: String,
+    val AddedTime: Int,
+    val AddedUser: String,
+    val BagTag: Any,
+    val CheckId: String,
+    val CheckLabel: String,
+    val EndDate: String,
+    val LastUpdatedDate: String,
+    val LastUpdatedUser: String,
+    val ReturnCode: String,
+    val StorageRoom: String,
+    val SyncDate: String,
+    val TransId: Int,
+    val ValidPeriod: String
+)
+
+
+
